9a13f9840c60f13828ca50bef6d567fb2f007b82
jackline chepngeno <chepngenojackline0@gmail.com>
2022-06-13 21:40:31 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+// your code here
+const submit = document.querySelector('form');
+submit.addEventListener("submit", (e) => {
+    e.preventDefault()
+    createToDo()
+    submit.reset()
+})
+
+function createToDo() {
+    const input = document.getElementById("new-task-description");
+    const newTask = document.createElement('li');
+    const deleteBtn = document.createElement('button');
+
+    newTask.textContent = `${input.value}       `;
+    document.getElementById('tasks').appendChild(newTask);
+
+
+    deleteBtn.textContent = "DONE";
+    newTask.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', () => newTask.remove());
+
+}; 
